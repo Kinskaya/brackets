e7c2343d362184d34a888e9a9aaf22bf5b678b0d
Kinskaya <kinskaya.nastya@gmail.com>
2021-03-04 22:29:29 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let b = {
+            open: bracketsConfig[i][0],
+            close: bracketsConfig[i][1]
+        }
+        arr.push(b);
+    }
+    for (let i = 0; i < 40; i++){
+        for(let j = 0; j < arr.length; j++) {
+            str = str.replace(arr[j].open + '' + arr[j].close, '');
+        }
+    }
+    if(str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
